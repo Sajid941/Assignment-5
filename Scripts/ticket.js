92c5687d7ae8c06ff68f4d4575bda2c904c37637
Sajid Islam <sajidislam941@gmail.com>
2024-02-19 15:13:57 +0600

    created dynamic price section

--- Scripts/ticket.js
+++ Scripts/ticket.js
@@ -1,6 +1,7 @@
-const allSeatBtn = [A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3, C4, D1, D2, D3, D4, E1, E2, E3, E4, F1, F2, F3, F4, G1, G2, G3, G4, H1, H2, H3, H4, I1, I2, I3, I4, J1, J2, J3, J4,]
+const allSeatBtn = document.querySelectorAll('.btn')
 let count = 0;
 let leftSeat = 40;
+let perTicket=0;
 for (const btn of allSeatBtn) {
     btn.addEventListener("click", function (event) {
         count++;
@@ -10,17 +11,24 @@ for (const btn of allSeatBtn) {
         const btnId = btn.innerText;
         const bgColorChange = document.getElementById(btnId);
         bgColorChange.classList.add('bg-[#1DD100]')
-        if(count ===5){
-                document.getElementById(btnId).disabled=true;
-        }
+        const seat=btn.innerText;
+        const priceList = document.getElementById('price-list');
+        let listElement = document.createElement("div");
+        let p = document.createElement("p");
+        p.innerText = seat;
+        let p2 = document.createElement("p");
+        p2.innerText = "Economoy";
+        let p3 = document.createElement("p");
+        p3.innerText = 550;
+        priceList.appendChild(listElement).classList.add('flex','justify-between');
+        listElement.append(p,p2,p3);
+        perTicket=perTicket+550;
+        setValueById('total-ticket-price',perTicket)
     })
 }
-const currentSeatElement = document.getElementById('seat-count').innerText;
-console.log(cu)
-console.log(currentSeat)
+
 // Reusable Function-------------->
 function setValueById(elementId, value) {
     const element = document.getElementById(elementId);
     element.innerText = value;
 }
-
